94ca44c2b77bf2bb841baa30d4a41bc7d714d0b6
LORDyyyyy <mlord6271@gmail.com>
2024-02-19 01:19:42 +0200

    copy buffer pwd edit

--- nvim/lua/lordyvim/plugs/utils.lua
+++ nvim/lua/lordyvim/plugs/utils.lua
@@ -3,7 +3,7 @@ function CopyFileDirectory()
     local file_path = vim.fn.expand('%:p')
     local directory = vim.fn.fnamemodify(file_path, ':h')
     
-    vim.fn.setreg('+', directory)
+    vim.fn.setreg('+', "\"" .. directory .. "\"")
     
     print("Directory copied to clipboard: " .. directory)
 end
